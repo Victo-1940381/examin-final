d70996bfdc6654c652fa2567d3bbba14dc079a48
ludo <1940381@carrefour.cegepvicto.ca>
2025-05-05 15:21:37 -0400

    fini morgan (log)?

--- serveur.js
+++ serveur.js
@@ -1,11 +1,16 @@
-import express from 'express';
+import express, { response } from 'express';
 import tacheRouter from './src/routes/tache.routes.js';
-
+import morgan  from 'morgan';
+import fs from 'fs';
+import path from 'path';
+import { error } from 'console';
 const app = express();
 app.use(express.json());
-
+app.use(morgan("dev", {
+    skip: function (req, res ) { return res.statusCode !=500},
+    stream: fs.createWriteStream('./error.log', { flags: 'a'})
+}));
 app.use(tacheRouter);
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`serveur démarré sur le port ${PORT}`);
